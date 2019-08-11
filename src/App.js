da150259a1078896fc5ce5fcb100016921390d4e
rachnabalani <balani.rachna@gmail.com>
2019-08-10 19:12:08 -0700

    minor comment added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ class App extends Component {
    constructor() {
        super();
        this.state = { displayBio: false};
-       this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+       this.toggleDisplayBio = this.toggleDisplayBio.bind(this); 
+       //we did this so that the toggleDisplayBio method below can access 'this' which will then have acecss to 'setState' from Component class.
+       // The toggleDisplayBio method on it's own doesn't have any meaning to 'this' keyword and it will throw an error "setState is undefined, this is undefined" 
+       // so basically we are saying that take this toggleDisplayBio function of declared here (that is being said on the left side of the equal sign),
+       // and then bind the "this" value to it. 
    }
 
    toggleDisplayBio() 
@@ -26,7 +30,7 @@ class App extends Component {
                 this.state.displayBio ? (
             <div>
             <p> I live in San Jose, CA </p>
-            <p> My favourite language is JavaScript and Java, and I think the React engine is pretty cool! </p>
+            <p> My favourite languages are JavaScript and Java, and I think the React engine is pretty cool! </p>
             <p> Besides coding, I like listening to podcasts, playing with my puppy Mac, and hike around California </p>
              <button onClick={this.toggleDisplayBio}> Show Less </button>
              </div>
