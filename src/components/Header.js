059d5828adfd2e6e0c7ea28a5ea9ff3ff818e3f8
rachnabalani <balani.rachna@gmail.com>
2020-02-17 20:16:21 -0800

    updating navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import navIcon from '../assets/menu_icon.svg';
 
 const Header = ({ children }) => {
-
-    const style = {display: 'inline-block', margin: 10, marginBottom: 30};
+    const style = {display: 'inline-block', margin: 10, marginBottom: 5};
     return(
         <div>
-            <div>
-             <h3 style={style}><Link to='/'>Home</Link></h3>
+             <div class='navMenu'>
+             {/* <h3 style={style}><Link to='/'>Home</Link></h3>
              <h3 style={style}><Link to='/about'>About</Link></h3>
               <h3 style={style}><Link to='/projects'>Highlighted Projects</Link></h3>
                <h3 style={style}><Link to='/Contact'>Contact</Link></h3>
-             {/* <h3 style={style}><Link to='/jokes'>Jokes</Link></h3>
+              <h3 style={style}><Link to='/jokes'>Jokes</Link></h3>
               <h3 style={style}><Link to='/music-master'>Music Master</Link></h3> */}
-             </div>
+              <img src={navIcon} alt = 'menu' style={{width : 30, height : 30}} />
+             </div> 
+
         {children}
         </div>
     )
